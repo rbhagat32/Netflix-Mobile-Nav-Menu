5c96022d1e71a2458f279221e90dac5957b46819
rbhagat32 <raghavbhagat32@gmail.com>
2024-04-29 15:12:14 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const open_btn = document.querySelector(".open-btn");
+const close_btn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
+
+open_btn.addEventListener("click", () => {
+  nav.forEach((nav_element) => nav_element.classList.add("visible"));
+});
+
+close_btn.addEventListener("click", () => {
+  nav.forEach((nav_element) => nav_element.classList.remove("visible"));
+});
